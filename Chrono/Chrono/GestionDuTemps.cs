bd6ed2445fce964cd65240c3ee75d5575d0acb60
Luka Moraiz <lmoraiz@iutbayonne.univ-pau.fr>
2018-01-21 23:13:10 +0100

    Ajoutez des fichiers projet.

---
+++ Chrono/Chrono/GestionDuTemps.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+
+namespace Chrono
+{
+    class GestionDuTemps
+    {
+    
+        static public int sec = 0 ;
+        static public int minutes = 0;
+        static public bool stop = false;
+
+
+        public static async Task LancerChrono()
+        {
+            stop = false;
+  
+            while (stop != true)
+            {
+                await Task.Delay(1000);
+                sec++;
+                if (sec == 60)
+                {
+                    sec = 0;
+                    minutes++;
+                }
+
+            }
+
+        }
+
+        public static void arreterChrono()
+        {
+            stop = true;
+        }
+
+    }
+}
---
+++ Chrono/Chrono/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Chrono
+{
+	public partial class MainPage : ContentPage
+	{
+        bool stop = false;
+        int sec = 0;
+        int min = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            minutesLabel.Text = min.ToString();
+            secLabel.Text = sec.ToString();
+
+        }
+
+        //CLICK SUR LE BOUTON START
+        async void BoutonLancerChrono_Clicked(object sender, EventArgs e)
+        {
+            stop = false;
+            while (stop != true)
+            {
+                await Task.Delay(1000);
+                sec++;
+                if (sec == 60)
+                {
+                    sec = 0;
+                    min++;
+                }
+                minutesLabel.Text = min.ToString();
+                secLabel.Text = sec.ToString();
+            }
+        }
+
+        //CLICK SUR LE BOUTON STOP
+        void BoutonStoperChrono_Clicked(object sender, EventArgs e)
+        {
+            stop = true;
+        }
+
+    }
+}
